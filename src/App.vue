596c32f2a5c07d58212d2778a1f2a3bc46b2018d
Pornput Sooduppatham <142374638+Pornput0012@users.noreply.github.com>
2025-02-25 14:08:06 +0700

    set up project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// Your setup code goes here
+</script>
+
+<template>
+  <div>
+    <!-- Your template code goes here -->
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="btn">Default</button>
+  </div>
+</template>
+
+<style scoped>
+/* Your styles go here */
+</style>
